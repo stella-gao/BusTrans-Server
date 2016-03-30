eda7f455536fb4fb25127bd2764e6f139ee37dd0
Stella Gao <stella.gao.89@gmail.com>
2016-03-30 18:42:17 -0400

    Update TestQuery.java

--- BusTrans-Server-V6/TestQuery.java
+++ BusTrans-Server-V6/TestQuery.java
@@ -27,9 +27,9 @@ public class TestQuery extends HttpServlet {
 	
 	String driver = "com.mysql.jdbc.Driver";
     String dbName = "sbs";
-    String dbPwd = "2CBEpBKZhw9q4sfV";
+    String dbPwd = "**************";
     String dbUserName = "sbs_user";
-    String url = "jdbc:mysql://128.235.90.197:3306/" + dbName;
+    String url = "jdbc:mysql://***.***.**.***:3306/" + dbName;
     String sqls = "select ptoBusArrT,podBusArrT from sbs_simpax_od where fid='%s' ";
     public static String fid = null;
 	/**
